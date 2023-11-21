541bc14fe4f416b19c665b7c47ffefcb3dcfac3a
Erwin Amador <eamador0305@gmail.com>
2023-11-21 10:26:57 -0600

    Add instance vars, constructor, methods, getters and setters for student class

--- student.rb
+++ student.rb
@@ -1,3 +1,12 @@
-class Student
+require_relative './person'
 
+class Student < Person
+  def initialize(age, name = "Unknown", parent_permission = "true", classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
