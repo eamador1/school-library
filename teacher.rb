781287d9b906bc5d8bff405da6ad349426d9c734
Erwin Amador <eamador0305@gmail.com>
2023-11-21 10:27:15 -0600

    Add instance vars, constructor, methods, getters and setters for teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,12 @@
-class Teacher
+require_relative './person'
 
+class Teacher < Person
+  def initialize(age,name = "Unknown", parent_permission = true, specialization)
+  super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
