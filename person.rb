03bdc437669e382a5e31837042cd6f6f5cc5377c
Erwin Amador <eamador0305@gmail.com>
2023-11-21 09:07:08 -0600

    Add instance vars, constructor, methods, getters and setters for person class

--- person.rb
+++ person.rb
@@ -1,3 +1,22 @@
 class Person
+  def initialize(name = "Unknown"  parent_permission = "True", age )
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+
+  attr_accessor :name, :age
+
+  private 
+  of_age?
+    @age >= 18
+  end
+
+  can_use_services?
+    @age>=18 || parent_permission = true
+  end
 
 end
