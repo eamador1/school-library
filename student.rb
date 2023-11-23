7685a71a7596c01f2aec7cd6db9599e0a0854d33
Erwin Amador <eamador0305@gmail.com>
2023-11-23 10:48:41 -0600

    create student belong to classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, name, parent_permission)
     @classroom = classroom
