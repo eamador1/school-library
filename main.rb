7624e5bc194e32386ed854b7d609f17ff9e9cedf
Erwin Amador <eamador0305@gmail.com>
2023-11-24 13:47:44 -0600

    create main class with its methods and hash

---
+++ main.rb
@@ -0,0 +1,37 @@
+require_relative 'app'
+
+CHOICES = {
+  1 => :list_all_books,
+  2 => :list_all_people,
+  3 => :create_a_person,
+  4 => :create_a_book,
+  5 => :create_a_rental,
+  6 => :list_all_rentals_for_given_person_id,
+  7 => :exit_app
+}.freeze
+
+def display_menu
+  puts "\nPlease choose an option by enteringf a number: "
+  CHOICES.each { |key, value| puts "#{key} - #{value.to_s.tr('_', ' ')}" }
+  gets.chomp.to_i
+end
+
+def handle_option(app, option)
+  choice = CHOICES[option]
+  if choice
+    app.send(choice)
+  else
+    puts 'Invalid option. Please choose a valid option'
+  end
+end
+
+def main
+  app = App.new
+
+  loop do
+    option = display_menu
+    handle_option(app, option)
+  end
+end
+
+main
