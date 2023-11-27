f52912296e0ada09c72d6009a02cf082348b70e9
Jose RM <mojica.raul@gmail.com>
2023-11-27 15:49:09 -0600

    Created menu_handler_module and moved exit_app method

--- app.rb
+++ app.rb
@@ -6,11 +6,12 @@ require_relative 'rental'
 require_relative 'classroom'
 require_relative 'display_module'
 require_relative 'create_instances_module'
+require_relative 'menu_handler_module'
 
 class App
   include DisplayModule
   include CreateInstancesModule
-
+  include MenuHandlerModule
   attr_accessor :books, :people, :rentals
 
   def initialize
