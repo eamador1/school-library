3bdfe77a7a53abf6d35a756cbd97ccf0e4fb083f
Jose RM <mojica.raul@gmail.com>
2023-11-29 12:05:57 -0600

    Fix issues of Rubocop

--- rental.rb
+++ rental.rb
@@ -1,5 +1,5 @@
 class Rental
-  attr_accessor :date, :book, :person
+  attr_accessor :date
 
   def initialize(date, book, person)
     @date = date
@@ -8,5 +8,37 @@ class Rental
 
     person.rentals << self
     book.rentals << self
+  end
+
+  def book=(book)
+    @book = book
+    book.add_rental(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.add_rental(self)
+  end
+
+  def to_h
+    {
+      'id' => @id,
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission
+    }
+  end
+
+  def to_json(*_args)
+    {
+      'date' => @date,
+      'book' => @book,
+      'person' => @person
+    }.to_json
+  end
+
+  def self.from_json(json_data)
+    data = JSON.parse(json_data)
+    new(data['date'], data['book'], data['person'])
   end
 end
