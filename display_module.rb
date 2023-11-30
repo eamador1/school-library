5e3d8872cd404730afd78f849dbbad9ea9806d1d
Erwin Amador <eamador0305@gmail.com>
2023-11-29 19:37:18 -0600

    load and display books from books.json

--- display_module.rb
+++ display_module.rb
@@ -1,6 +1,25 @@
 module DisplayModule
   def list_all_books
-    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    stored_books = load_books_from_json
+
+    if stored_books.empty?
+      puts 'No books found in the file.'
+    else
+      stored_books.each_with_index do |book, index|
+        puts "#{index}) Title: \"#{book['title']}\", Author: #{book['author']}"
+      end
+    end
+  end
+
+  def load_books_from_json
+    stored_books = []
+
+    if File.exist?('books.json')
+      existing_books_json = File.read('books.json')
+      stored_books = JSON.parse(existing_books_json) unless existing_books_json.empty?
+    end
+
+    stored_books
   end
 
   def list_all_people
